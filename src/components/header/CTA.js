4d22f610b5c156b342d8005ec6e3e255d8fea096
Virat Patel <virat.patel@ecoatm.com>
2023-03-27 23:52:57 +0530

    Strap

---
+++ src/components/header/CTA.js
@@ -0,0 +1,18 @@
+import React from "react";
+import CV from "../../assets/cv_meri_gogichashvili.pdf";
+
+const CTA = () => {
+  return (
+    <div className="cta">
+      <a href={CV} download className="btn">
+        Download Report
+      </a>
+      <a href="#contact" className="btn btn-primary">
+        {/* Let's talk */}
+        Contact Us
+      </a>
+    </div>
+  );
+};
+
+export default CTA;
